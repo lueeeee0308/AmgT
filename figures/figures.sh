05e3c9da16b649b3d4f41230e2200b8a1cbf1501
lueeeee0308 <18811030092@163.com>
2024-04-17 16:36:07 +0800

    Add files via upload

--- figures/figures.sh
+++ figures/figures.sh
@@ -4,8 +4,8 @@ python3 parse_amgt.py ../hypre_test/data/Mixed.out > mix.csv
 python3 merge_pro.py
 python3 Fig7.py
 
-# python3 Fig8.py
+python3 Fig8.py
 
 
-# python3 preprocess_parse.py > preprocess_test.csv
-# python3 Fig9.py
+python3 preprocess_parse.py > preprocess_test.csv
+python3 Fig9.py
